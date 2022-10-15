3cff4f26abddaff5c02fc8d41e5cdd36be1fa0e8
Jon Jagger <jon@jaggersoft.com>
2022-10-15 16:17:24 +0100

    Fix tid make target

--- scripts/print_test_id.sh
+++ scripts/print_test_id.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -Eeu
 
-export MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly ROOT_DIR="$(cd "${MY_DIR}/.." && pwd)"
+
 source "${MY_DIR}/echo_env_vars.sh"
 export $(echo_env_vars)
 
@@ -12,5 +14,6 @@ docker run \
   --interactive \
   --rm \
   --tty \
+  --volume="${ROOT_DIR}/test:/${XY_DIR}/test:ro" \
   "${XY_IMAGE}" \
     "/${XY_DIR}/test/print_id.py"
