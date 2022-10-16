988ab97a2ed5e79ffcf416caf1905b432227121d
Jon Jagger <jon@jaggersoft.com>
2022-10-16 06:02:42 +0100

    Add failing test

--- test/unit/test_score.py
+++ test/unit/test_score.py
@@ -3,36 +3,57 @@ from model import company_score
 EXPECTED = {
     'invalid_entry': 76,
     'all_Xs_and_Ys': 82,
-    'letters_from_heading': 1850
+    'letters_from_heading': 1850,
+    'only_lowercase_letters_from_heading': 18500
 }
 
 
 def test_invalid_entry():
     a, b, c, d = go('XXYYY'), go('XYYXX'), go('XYYXY'), go('?YYXX')
     scores = company_score(a, b, c, d, is_sentence=False, is_profound=False)
-    assert scores == [[4, 8, 5, 3, 3], [4, 3, 5, 4, 4], [4, 3, 5, 4, 3], [-2, 3, 5, 4, 4]]
+    assert scores == [[4, 8, 5, 3, 3],
+                      [4, 3, 5, 4, 4],
+                      [4, 3, 5, 4, 3],
+                      [-2, 3, 5, 4, 4]]
     assert sum(flatten(scores)) == EXPECTED['invalid_entry']
 
 
 def test_all_Xs_and_Ys():
     a, b, c, d = go('XXYYY'), go('XYYXX'), go('XYYXY'), go('XYYXX')
     scores = company_score(a, b, c, d, is_sentence=False, is_profound=False)
-    assert scores == [[4, 8, 5, 3, 3], [4, 3, 5, 4, 4], [4, 3, 5, 4, 3], [4, 3, 5, 4, 4]]
+    assert scores == [[4, 8, 5, 3, 3],
+                      [4, 3, 5, 4, 4],
+                      [4, 3, 5, 4, 3],
+                      [4, 3, 5, 4, 4]]
     assert sum(flatten(scores)) == EXPECTED['all_Xs_and_Ys']
 
 
 def test_letters_from_heading():
     a, b, c, d = go('BigFi'), go('shLit'), go('tlePo'), go('ndWaR')
     scores = company_score(a, b, c, d, is_sentence=False, is_profound=False)
-    assert scores == [[1, -1, 1, 2, -1], [4, 8, 16, -1, 32], [0, 0, -1, 0, -1], [0, 256, 512, -1, 1024]]
+    assert scores == [[1, -1, 1, 2, -1],
+                      [4, 8, 16, -1, 32],
+                      [0, 0, -1, 0, -1],
+                      [0, 256, 512, -1, 1024]]
     assert sum(flatten(scores)) == EXPECTED['letters_from_heading']
+
+
+def X_test_only_lowercase_letters_from_heading():
+    a, b, c, d = go('bigfi'), go('shlit'), go('tlepo'), go('ndwar')
+    scores = company_score(a, b, c, d, is_sentence=False, is_profound=False)
+    assert scores == [[10, -10, 10, 20, -10],
+                      [40, 80, 160, -10, 320],
+                      [0, 0, -10, 0, -10],
+                      [0, 2560, 5120, -10, 10240]]
+    assert sum(flatten(scores)) == EXPECTED['only_lowercase_letters_from_heading']
 
 
 def test_scores_increase_as_fortune_cookies_are_unlocked():
     s0 = EXPECTED['invalid_entry']
     s1 = EXPECTED['all_Xs_and_Ys']
     s2 = EXPECTED['letters_from_heading']
-    assert is_sorted([s0, s1, s2]), 'not sorted!'
+    s3 = EXPECTED['only_lowercase_letters_from_heading']
+    assert is_sorted([s0, s1, s2, s3]), 'not sorted!'
 
 
 def go(s):
