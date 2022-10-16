6d28a289585b6f0533a9b361e624a08a74d4f809
Jon Jagger <jon@jaggersoft.com>
2022-10-16 13:06:24 +0100

    Add shellcheck disable=SC2116 to bash_shortcuts.sh

--- scripts/bash_shortcuts.sh
+++ scripts/bash_shortcuts.sh
@@ -3,18 +3,28 @@
 
 rut()
 {
+  # shellcheck disable=SC2116
   local -r TIDS=$(echo "${*// / or /}")
   make run_unit_tests TIDS="-k ${TIDS}"
 }
 
 eut()
 {
+  # shellcheck disable=SC2116
   local -r TIDS=$(echo "${*// / or /}")
   make exec_unit_tests TIDS="-k ${TIDS}"
 }
 
 rst()
 {
+  # shellcheck disable=SC2116
   local -r TIDS=$(echo "${*// / or /}")
   make run_system_tests TIDS="-k ${TIDS}";
 }
+
+# Note: replacing
+#    local -r TIDS=$(echo "${*// / or /}")
+# with
+#    local -r TIDS="$(*// / or /)"
+# causes warning
+#    rut:N: permission denied: __pycache__//
