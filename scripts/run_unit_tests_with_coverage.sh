4db09d5ebbd3491e772ee6719aac6ba1dd1ea7fd
Jon Jagger <jon@jaggersoft.com>
2022-10-16 11:43:49 +0100

    Add missing network_up call to bash script

--- scripts/run_unit_tests_with_coverage.sh
+++ scripts/run_unit_tests_with_coverage.sh
@@ -6,6 +6,7 @@ source "${MY_DIR}/echo_env_vars.sh"
 export $(echo_env_vars)
 
 "${MY_DIR}/build_image.sh"
+"${MY_DIR}/network_up.sh"
 "${MY_DIR}/server_up.sh"
 "${MY_DIR}/wait_till_server_ready.sh"
 "${MY_DIR}/exec_unit_tests_with_coverage.sh"
