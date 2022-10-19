95715143fbeca86fad5a7443aea08cc7ef0ba8c8
Jon Jagger <jon@jaggersoft.com>
2022-10-19 18:23:15 +0100

    Start preparing scores.html

--- server/app/score.py
+++ server/app/score.py
@@ -6,3 +6,7 @@ def register_score_routes(app_blueprint):
     @app_blueprint.route('/score')
     def score():
         return render_template('score.html')
+
+    @app_blueprint.route('/scores')
+    def scores():
+        return render_template('scores.html')
