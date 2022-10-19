96b1e1e4b5742dac09eef96284510d000e88b9b3
Jon Jagger <jon@jaggersoft.com>
2022-10-19 19:56:03 +0100

    Add test for t fixture

---
+++ test/unit/test_t.py
@@ -0,0 +1,9 @@
+
+def test_d4f4f0(t):
+    """
+    Using a t fixture means you can write the
+    test description in the docstring, like this,
+    in free form, of any length you like, over
+    as many lines as you like.
+    """
+    assert t.id == "d4f4f0"
