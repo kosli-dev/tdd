c6fec3d7337a4ef2fa0466f22cb3e11cc97eef07
Jon Jagger <jon@jaggersoft.com>
2022-10-19 19:58:50 +0100

    Add test for t fixture with marked.parameterize data

--- test/unit/test_t.py
+++ test/unit/test_t.py
@@ -1,3 +1,7 @@
+import pytest
+
+MARKED_PARAMS = []
+
 
 def test_d4f4f0(t):
     """
@@ -7,3 +11,16 @@ def test_d4f4f0(t):
     as many lines as you like.
     """
     assert t.id == "d4f4f0"
+
+
+@pytest.mark.parametrize('arg', [1, 2, 3, 4])
+def test_d4f4f1(t, arg):
+    """
+    When a test has a t fixture and has parameterized data
+    then t will be passed multiple times, so provide t.n which
+    is unique each time the test is run in case the tests need
+    to use this to ensure they are truly isolated from each other
+    """
+    MARKED_PARAMS.append(t.n)
+    if len(MARKED_PARAMS) == 4:
+        assert sorted(MARKED_PARAMS) == [1, 2, 3, 4]  # parameters are in random order
