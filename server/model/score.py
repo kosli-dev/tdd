e942714a41d5adc7f992590e6d27b95044c3f2ca
Jon Jagger <jon@jaggersoft.com>
2022-10-20 08:03:05 +0100

    Move large comment into test file ready to convert into a test

--- server/model/score.py
+++ server/model/score.py
@@ -32,44 +32,6 @@ def squad_score(n, squad_decisions, company_decisions):
 
 
 def letter_score(n, squad_decision, company_decisions):
-    """
-    XY Scoring Table is as follows:
-                          X Y
-    All squads play Y     0 5
-    One squad plays Y     8 3
-    Two+ squads play Y    4 3
-
-    The important property of this table is that for
-    a company of 3 or more squads choosing all Y's gives
-    the biggest company score.
-
-    3 squads
-       0X,3Y ==   0 + 3*5 == 0+15 == 15 <===
-       1X,2Y == 1*8 + 2*3 == 8+6  == 14
-       2X,1Y == 2*4 + 1*3 == 8+3  == 11
-       3X,0Y == 3*4 +   0 == 12+0 == 12
-    4 squads
-       0X,4Y ==   0 + 4*5 == 0+20 == 20 <===
-       1X,3Y == 1*8 + 3*3 == 8+9  == 17
-       2X,2Y == 2*4 + 2*3 == 8+6  == 14
-       3X,1Y == 1*4 + 3*3 == 4+9  == 13
-       4X,0Y == 4*4 +   0 == 16+0 == 16
-    5 squads
-       0X,5Y ==   0 + 5*5 == 0+25 == 25 <===
-       1X,4Y == 1*8 + 4*3 == 8+12 == 20
-       2X,3Y == 2*4 + 3*3 == 8+9  == 17
-       3X,2Y == 3*4 + 2*3 == 12+6 == 18
-       4X,1Y == 4*4 + 1*3 == 16+3 == 19
-       5X,0Y == 5*4 +   0 == 20+0 == 20
-    6 squads
-       0X,6Y ==   0 + 6*5 == 0+30 == 30 <===
-       1X,5Y == 1*8 + 5*3 == 8+15 == 23
-       2X,4Y == 2*4 + 4*3 == 8+12 == 20
-       3X,3Y == 3*4 + 3*3 == 12+9 == 21
-       4X,2Y == 4*4 + 2*3 == 16+6 == 22
-       5X,1Y == 5*4 + 1*3 == 20+3 == 23
-       6X,0Y == 6*4 +   0 == 24+0 == 24
-    """
     if is_invalid_or_illegal(squad_decision):
         score, n = -2, 0
     elif is_vowel(squad_decision):
@@ -95,9 +57,8 @@ MARKERS = {
   # Jerry's version uses X* for illegal or illegible decisions
   # and Y* for unscoring consonants. I have changed these to
   # '?' and '0' respectively as using single chars simplifies
-  # the program, specifically the ability to write
-  #    word.chars
-  # to get the 5 decision-letters from a 5 character word.
+  # the program, specifically the ability to get 5 decision-letters
+  # from a 5 character word.
   INVALID_OR_ILLEGAL:  '?',
   UNSCORING_CONSONANT: '0'
 }
@@ -159,6 +120,7 @@ BIG_FISH_TABLE = {
 
 
 def marked(company_decisions):
+    # ...This is the original Ruby definition
     # company_decisions.map do |squad_decisions|
     #   squad_decisions.map do |decision|
     #     if invalid_or_illegal?(decision)
--- test/unit/test_score.py
+++ test/unit/test_score.py
@@ -135,6 +135,46 @@ def test_86b909():
                       [51200000, -100000, 102400000, 0, 0]]
 
 
+"""
+XY Scoring Table is as follows:
+                      X Y
+All squads play Y     0 5
+One squad plays Y     8 3
+Two+ squads play Y    4 3
+
+The important property of this table is that for
+a company of 3 or more squads choosing all Y's gives
+the biggest company score.
+
+3 squads
+   0X,3Y ==   0 + 3*5 == 0+15 == 15 <===
+   1X,2Y == 1*8 + 2*3 == 8+6  == 14
+   2X,1Y == 2*4 + 1*3 == 8+3  == 11
+   3X,0Y == 3*4 +   0 == 12+0 == 12
+4 squads
+   0X,4Y ==   0 + 4*5 == 0+20 == 20 <===
+   1X,3Y == 1*8 + 3*3 == 8+9  == 17
+   2X,2Y == 2*4 + 2*3 == 8+6  == 14
+   3X,1Y == 1*4 + 3*3 == 4+9  == 13
+   4X,0Y == 4*4 +   0 == 16+0 == 16
+5 squads
+   0X,5Y ==   0 + 5*5 == 0+25 == 25 <===
+   1X,4Y == 1*8 + 4*3 == 8+12 == 20
+   2X,3Y == 2*4 + 3*3 == 8+9  == 17
+   3X,2Y == 3*4 + 2*3 == 12+6 == 18
+   4X,1Y == 4*4 + 1*3 == 16+3 == 19
+   5X,0Y == 5*4 +   0 == 20+0 == 20
+6 squads
+   0X,6Y ==   0 + 6*5 == 0+30 == 30 <===
+   1X,5Y == 1*8 + 5*3 == 8+15 == 23
+   2X,4Y == 2*4 + 4*3 == 8+12 == 20
+   3X,3Y == 3*4 + 3*3 == 12+9 == 21
+   4X,2Y == 4*4 + 2*3 == 16+6 == 22
+   5X,1Y == 5*4 + 1*3 == 20+3 == 23
+   6X,0Y == 6*4 +   0 == 24+0 == 24
+"""
+
+
 def test_86b920():
     """ Scores increase as fortune cookies levels are unlocked"""
     s1 = EXPECTED['invalid_entry']
