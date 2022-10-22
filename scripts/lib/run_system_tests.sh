2632039438a4b60392b4697e52370c226ab8a812
Jon Jagger <jon@jaggersoft.com>
2022-10-22 14:42:36 +0100

    Drop leading slash in XY_APP_DIR env-var uses

--- scripts/lib/run_system_tests.sh
+++ scripts/lib/run_system_tests.sh
@@ -9,7 +9,7 @@ docker run \
   --net "${XY_NETWORK}" \
   --rm \
   --tty \
-  --volume="${XY_REPO_DIR}:/${XY_APP_DIR}" \
+  --volume="${XY_REPO_DIR}:${XY_APP_DIR}" \
   "${XY_IMAGE}" \
     "${XY_APP_DIR}/test/system/run.sh"
 set -e
