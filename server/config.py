9518c792d4d7e28555e6cdbeeb209fba7b32122e
Jon Jagger <jon@jaggersoft.com>
2022-10-27 06:57:46 +0100

    Delete dead swagger setting from Config

--- server/config.py
+++ server/config.py
@@ -8,4 +8,3 @@ def raise_(ex):  # pragma no cover
 class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or raise_(Exception('SECRET_KEY not set'))
-    SWAGGER_USE_HTTPS = os.environ.get('SWAGGER_USE_HTTPS') == 'TRUE'
