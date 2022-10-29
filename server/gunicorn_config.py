5329246f310f74263995a964edf267908cb5fee0
Jon Jagger <jon@jaggersoft.com>
2022-10-29 20:03:12 +0100

    Add if statements to gunicorn handlers to get demo working again

--- server/gunicorn_config.py
+++ server/gunicorn_config.py
@@ -4,11 +4,13 @@ from coverage import Coverage
 def post_worker_init(worker):
     #print(f"post_worker_init(): {worker.pid=}", flush=True)
     cov = Coverage.current()
-    cov.start()
+    if cov:
+        cov.start()
 
 
 def worker_exit(server, worker):
     #print(f"worker_exit(): {server.pid=}, {worker.pid=}", flush=True)
     cov = Coverage.current()
-    cov.stop()
-    cov.save()  # pragma: no cover
+    if cov:
+        cov.stop()
+        cov.save()  # pragma: no cover
