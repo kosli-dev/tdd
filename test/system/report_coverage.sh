537e4bbcc2f13a8c3fd8174bf3fc1bb696260edd
Jon Jagger <jon@jaggersoft.com>
2022-10-29 18:38:38 +0100

    Update file documenting inconsistent coverage

--- test/system/report_coverage.sh
+++ test/system/report_coverage.sh
@@ -2,15 +2,7 @@
 
 cd "${XY_APP_DIR}"
 
-status=42
-while [ ${status} != 0 ];
-do
-  # We often get the following error
-  #   Couldn't use data file '/xy/.coverage': unable to open database file
-  # Empirically, if we keep trying it becomes ready.
-  coverage combine "${XY_APP_DIR}" # &> /dev/null
-  status=$?
-done
+coverage combine "${XY_APP_DIR}"
 
 coverage json > /dev/null
 percent=$(cat "${XY_APP_DIR}/coverage.json" | jq .totals.percent_covered)
