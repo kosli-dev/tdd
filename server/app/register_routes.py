bfb8225bac0935868475a71bcf9ed01a97eccf66
Jon Jagger <jon@jaggersoft.com>
2022-11-03 14:03:27 +0100

    Get squad data from form and score it

--- server/app/register_routes.py
+++ server/app/register_routes.py
@@ -18,8 +18,7 @@ def register_routes(app_blueprint):
         form = ScoreForm(obj=org)
         if form.validate_on_submit():
             form.populate_obj(org)
-            # TODO: Get actual form's data
-            sid = write_result(FAKE_FORM_DATA)
+            sid = write_result(data_from(form))
             return redirect(url_for('app.scores', sid=sid))
         else:
             return render_template('score.html', form=form)
@@ -29,12 +28,18 @@ def register_routes(app_blueprint):
         return render_template('scores.html', result=read_result(sid))
 
 
-FAKE_FORM_DATA = {
-    "squads": [
-        {"char": "A", "letters": "xyzzy", "is_word": False},
-        {"char": "B", "letters": "hello", "is_word": True},
-        {"char": "C", "letters": "world", "is_word": True},
-    ],
-    "is_sentence": False,
-    "is_profound": False
-}
+def data_from(form):
+    data = {
+        "squads": [],
+        "is_sentence": form.is_sentence.data,
+        "is_profound": form.is_profound.data
+    }
+
+    for i, squad in enumerate(form.squads):
+        data["squads"].append({
+            "char": "ABCDEF"[i],
+            "letters": squad.letters.data,
+            "is_word": squad.is_word.data
+        })
+
+    return data
