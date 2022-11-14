cc17239f4b3c95f037bd124f5a0ca17ee827ff27
Jon Jagger <jon@jaggersoft.com>
2022-11-14 06:07:24 +0000

    Rename variable in gunicorn.sh

--- server/gunicorn.sh
+++ server/gunicorn.sh
@@ -4,18 +4,16 @@ set -Eeu
 readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
 
 if [ -z "${COVERAGE_PROCESS_START:-}" ]; then
-  echo "Not setting up coverage configuration"
-  CONFIG_FLAG=
+  COVERAGE_CONFIG=
 else
-  echo "Setting up coverage configuration"
-  CONFIG_FLAG="--config ${MY_DIR}/gunicorn_config.py"
+  COVERAGE_CONFIG="--config ${MY_DIR}/gunicorn_coverage.py"
 fi
 
 exec gunicorn \
   --access-logfile - \
   --access-logformat '%(h)s %(l)s %(t)s %(M)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
   --bind="0.0.0.0:${XY_PORT}" \
-  ${CONFIG_FLAG} \
+  ${COVERAGE_CONFIG} \
   --log-level=info \
   --threads=4 \
   --workers="${XY_WORKERS}" \
