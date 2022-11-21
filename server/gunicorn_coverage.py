1c22ec6b82329e925393ff8304e5d30072a09beb
Jon Jagger <jon@jaggersoft.com>
2022-11-21 10:00:20 +0000

    Don't create coverage object in gunicorn_coverage.py unless inside test server

--- server/gunicorn_coverage.py
+++ server/gunicorn_coverage.py
@@ -7,11 +7,16 @@ def xy_app_dir():
     return os.environ.get("XY_APP_DIR")
 
 
-def config_file_path():
+def coverage_rc_file_path():
     return f"{xy_app_dir()}/test/system/.coveragerc"
 
 
-cov = coverage.Coverage(config_file=config_file_path())
+def coverage_on_server():
+    return os.environ.get("COVERAGE_PROCESS_START", None)
+
+
+if coverage_on_server():
+    cov = coverage.Coverage(config_file=coverage_rc_file_path())
 
 
 def post_fork(server, worker):
@@ -27,10 +32,6 @@ def worker_exit(server, worker):
             cov.save()
         except:
             pass
-
-
-def coverage_on_server():
-    return os.environ.get("COVERAGE_PROCESS_START", None)
 
 
 def recreate_coverage_dir():
