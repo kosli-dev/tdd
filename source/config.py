ab52dd516a32931b6d2ee43378be8b8131c5a019
Jon Jagger <jon@jaggersoft.com>
2022-12-05 09:54:55 +0000

    Add WTF_CSRF_ENABLED env-var to config

--- source/config.py
+++ source/config.py
@@ -5,6 +5,15 @@ def raise_(ex):  # pragma no cover
     raise ex
 
 
+def wtf_csrf_enabled():
+    env_var = os.environ.get('WTF_CSRF_ENABLED', True)
+    if env_var == 'False':  # pragma no cover
+        env_var = False
+    return env_var
+
+
 class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or raise_(Exception('SECRET_KEY not set'))
+
+    WTF_CSRF_ENABLED = wtf_csrf_enabled()
