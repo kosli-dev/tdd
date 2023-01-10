3d1531ede83e9a19692a0145b44554dd55e0a494
Jon Jagger <jon@jaggersoft.com>
2023-01-10 16:27:02 +0000

    Upgrade requirements.txt; switch from pytest-parallel to pytest-xdist

--- test/system/run.sh
+++ test/system/run.sh
@@ -4,11 +4,11 @@ pytest \
   --capture=no \
   --color=yes \
   --ignore="${XY_CONTAINER_ROOT_DIR}/test/unit" \
+  -n auto \
   --no-cov \
   -o cache_dir=/tmp \
   --pythonwarnings=error \
   --quiet \
   --random-order-bucket=global \
   --tb=short \
-  --workers auto \
     "${TIDS}"
