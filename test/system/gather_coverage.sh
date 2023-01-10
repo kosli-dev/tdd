ddd761b2a86cb8fb4d0c8b026f0bffbe3ddde5a9
Jon Jagger <jon@jaggersoft.com>
2023-01-10 10:53:29 +0000

    Delete dead comments

--- test/system/gather_coverage.sh
+++ test/system/gather_coverage.sh
@@ -4,15 +4,11 @@ readonly COV_DIR="${1}"
 
 actual_coverage_files_count()
 {
-  # Find is less noisy than ls when there are no matches
   find "${COV_DIR}" -maxdepth 1 -type f -name ^.coverage | wc -l | xargs
 }
 
 wait_for_all_coverage_files_based_on_stabilizing()
 {
-  # Occasionally you might want to wait until the number of
-  # .coverage files stabilizes. Eg you are working without
-  # a network connection and cannot do a `docker build`.
   while : ; do
     echo -n .
     a1="$(actual_coverage_files_count)"; sleep 0.25
@@ -44,30 +40,8 @@ create_coverage_html()
     --quiet
 }
 
-#wait_for_all_coverage_files_based_on_workers_count
 wait_for_all_coverage_files_based_on_stabilizing
 cd "${COV_DIR}"
 coverage combine --keep --quiet
 create_coverage_json
 create_coverage_html
-
-
-
-#wait_for_all_coverage_files_based_on_workers_count() {
-#  # We have sent a SIGHUP to the gunicorn master which will restart
-#  # each worker. Now we wait for each workers' sigterm handler to
-#  # write its .coverage file.
-#  echo -n Waiting for coverage files
-#  local -r max_tries=20
-#  for i in $(seq "${max_tries}"); do
-#    echo -n .
-#    if [ "$(actual_coverage_files_count)" == "${XY_WORKER_COUNT}" ]; then
-#      echo
-#      return 0
-#    else
-#      sleep 0.1
-#    fi
-#  done
-#  echo
-#  echo "Gave up waiting for all .coverage files after "${max_tries}" tries"
-#}
