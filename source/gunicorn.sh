b45f7dff4ea649e6329775499ba35b86151f7e72
Jon Jagger <jon@jaggersoft.com>
2023-01-10 15:55:04 +0000

    Drop unneeded exec from gunicorn.sh

--- source/gunicorn.sh
+++ source/gunicorn.sh
@@ -9,7 +9,7 @@ else
   COVERAGE_CONFIG="--config ${MY_DIR}/gunicorn_coverage.py"
 fi
 
-exec gunicorn \
+gunicorn \
   --access-logfile - \
   --access-logformat '%(h)s %(l)s %(t)s %(M)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
   --bind="0.0.0.0:${XY_CONTAINER_PORT}" \
