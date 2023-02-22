1becd601e467e5daa5da801ba3f27779811f7094
Jon Jagger <jon@jaggersoft.com>
2023-02-22 07:26:57 +0100

    Minor refactoring

--- source/strangler/switch.py
+++ source/strangler/switch.py
@@ -5,52 +5,42 @@ individual method/property level via keyword arguments:
   Properties: (..., *, getter, setter)
 
 use/getter/setter are Tuples with 3 elements
-  [0] controls if the Old call is made
-  [1] controls if the New call is made
-  [2] controls which is 'main' if Old _and_ New calls are being made:
+  [0] controls which is 'main' if Old _and_ New calls are being made:
+  [1] controls if the Old call is made
+  [2] controls if the New call is made
 
 if Old/New calls exhibit _different_ behaviour
   if running-unit-tests:
       raise StrangledDifference(...)
   else:
-      log difference to a file
-
-On staging and production, while strangling is in progress
-differences are logged to a file, they do NOT raise an exception.
-However, inside unit-tests, differences do raise an exception.
+      log difference to a file - never leak an exception
 """
 
-# Steps in moving from Old to New...
-
-OLD_ONLY = (True, False, "old")  # 1st - Old call only, returns Old
-OLD_MAIN = (True, True, "old")   # 2nd - both called, returns Old
-NEW_MAIN = (True, True, "new")   # 3rd - both called, returns New
-NEW_ONLY = (False, True, "new")  # 4th - New call only, returns New
+OLD_ONLY = ("old", True, False)  # mainline=old, Call old only
+OLD_MAIN = ("old", True, True)   # mainline=old, Call both
+NEW_MAIN = ("new", True, True)   # mainline=new, Call both
+NEW_ONLY = ("new", False, True)  # mainline=new, Call new only
 
 
 def switches():
     return [OLD_ONLY, OLD_MAIN, NEW_ONLY, NEW_MAIN]
 
 
+def old_is_main(use):
+    return use[0] == 'old'
+
+
+def new_is_main(use):
+    return use[0] == 'new'
+
+
 def call_old(use):
-    return use[0]
+    return use[1]
 
 
 def call_new(use):
-    return use[1]
+    return use[2]
 
 
 def call_both(use):
     return call_old(use) and call_new(use)
-
-
-def old_is_main(use):
-    return _main_is(use, 'old')
-
-
-def new_is_main(use):
-    return _main_is(use, 'new')
-
-
-def _main_is(use, age):
-    return use[2] == age
